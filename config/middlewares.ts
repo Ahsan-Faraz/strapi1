08f056b560abc0f37f6d14f8c185bbaf29b283d5
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2025-12-22 01:31:33 +0500

    Fix CORS and security middleware config for Railway

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,8 +1,28 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': ["'self'", 'data:', 'blob:', 'https:'],
+          'media-src': ["'self'", 'data:', 'blob:', 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      origin: ['*'],
+      headers: ['*'],
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
