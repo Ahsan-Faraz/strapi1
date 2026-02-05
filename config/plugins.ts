93c96e1e261de0a742942ccf38a8db74bef27a84
ahsanfaraz-WebDev <ahsanfaraza674@gmail.com>
2026-02-06 02:43:58 +0500

    Add strapi-page-builder plugin config with API key

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,6 +2,12 @@ export default ({ env }) => ({
   seo: {
     enabled: true,
   },
+  'strapi-page-builder': {
+    enabled: true,
+    config: {
+      apiKey: env('STRAPI_PAGE_BUILDER_API_KEY', ''),
+    },
+  },
   'users-permissions': {
     config: {
       jwt: {
